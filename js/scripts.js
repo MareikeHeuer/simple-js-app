779efe63dc277022b8fa92e9b64ee616f1eea98d
MareikeHeuer <51022514+MareikeHeuer@users.noreply.github.com>
2020-04-23 22:51:21 +0200

    Task 1.2 JS
    Pokemon List: Created list of my favorite Pokemons from heaviest to lightest (weight in kg)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,59 @@
-alert('Hello world');
+var pokemonList = [
 
-var favoriteFood = 'Asian Food'
-document.write(favoriteFood)
+  // List of Pokemons from heaviest weight to lightest weight
+  {
+    name: 'Spheal',
+    heightInMeters: 0.8,
+    weightInKg: 39.5,
+    abilities: ['oblivious', 'thick-fat', 'ice-body']
+  }
+
+  {
+    name: 'Togekiss',
+    heightInMeters: 1.5,
+    weightInKg: 38,
+    abilities: ['serene-grace', 'hustle', 'super-luck']
+  }
+
+  {
+    name: 'Happiny',
+    heightInMeters: 0.6,
+    weightInKg: 24.4,
+    abilities: ['natural-cure', 'serene-grace', 'friend-guard']
+  }
+
+  {
+    name: 'Teddiursa',
+    heightInMeters: 0.6,
+    weightInKg: 8.8,
+    abilities: ['pickup', 'quick-feet', 'honey-gather']
+  }
+
+  {
+    name: 'Pikachu',
+    heightInMeters: 0.4,
+    weightInKg: 6,
+    abilities: ['static', 'lightningrod']
+  },
+
+  {
+    name: 'Jigglypuff',
+    heightInMeters: 0.5,
+    weightInKg: 5.5,
+    abilities: ['cute-charm', 'friend-guard']
+  }
+
+  {
+    name: 'Minun',
+    heightInMeters: 0.4,
+    weightInKg: 4.2,
+    abilities: ['minus']
+  }
+
+  {
+    name: 'Cherubi',
+    heightInMeters: 0.4,
+    weightInKg: 3.3,
+    abilities: ['chlorophyll']
+  }
+]
